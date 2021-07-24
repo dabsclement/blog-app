a4ae6d8453304b4c5f573eaafef1f300effa85eb
oluwaseun <dadsclement@gmail.com>
2021-07-24 12:24:03 +0100

    NavBar Component done

---
+++ src/pages/Home.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+        Hello, Welcome to my Blog Page!
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et lorem
+        molestie, pretium erat vitae, congue nisl. Vestibulum consequat libero
+        in risus porta tincidunt. Pellentesque lacinia, nulla et porttitor
+        consequat, neque leo commodo justo, sollicitudin dignissim lectus mauris
+        vel enim. Proin sed turpis magna. Vivamus risus erat, varius ac magna
+        sit amet, consequat facilisis velit. Praesent condimentum velit sit amet
+        nunc pharetra, id malesuada turpis tempor. Curabitur turpis nisl,
+        iaculis placerat suscipit sed, venenatis et velit. Sed tristique, sapien
+        vel faucibus vehicula, erat leo molestie magna, id vestibulum sapien
+        ligula at lorem. Duis sagittis quis nunc imperdiet vulputate. Sed nunc
+        nisl, imperdiet congue tristique egestas, mollis et metus. Donec aliquam
+        molestie enim at viverra. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Fusce vel nunc id
+        augue dictum interdum.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et lorem
+        molestie, pretium erat vitae, congue nisl. Vestibulum consequat libero
+        in risus porta tincidunt. Pellentesque lacinia, nulla et porttitor
+        consequat, neque leo commodo justo, sollicitudin dignissim lectus mauris
+        vel enim. Proin sed turpis magna. Vivamus risus erat, varius ac magna
+        sit amet, consequat facilisis velit. Praesent condimentum velit sit amet
+        nunc pharetra, id malesuada turpis tempor. Curabitur turpis nisl,
+        iaculis placerat suscipit sed, venenatis et velit. Sed tristique, sapien
+        vel faucibus vehicula, erat leo molestie magna, id vestibulum sapien
+        ligula at lorem. Duis sagittis quis nunc imperdiet vulputate. Sed nunc
+        nisl, imperdiet congue tristique egestas, mollis et metus. Donec aliquam
+        molestie enim at viverra. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Fusce vel nunc id
+        augue dictum interdum.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et lorem
+        molestie, pretium erat vitae, congue nisl. Vestibulum consequat libero
+        in risus porta tincidunt. Pellentesque lacinia, nulla et porttitor
+        consequat, neque leo commodo justo, sollicitudin dignissim lectus mauris
+        vel enim. Proin sed turpis magna. Vivamus risus erat, varius ac magna
+        sit amet, consequat facilisis velit. Praesent condimentum velit sit amet
+        nunc pharetra, id malesuada turpis tempor. Curabitur turpis nisl,
+        iaculis placerat suscipit sed, venenatis et velit. Sed tristique, sapien
+        vel faucibus vehicula, erat leo molestie magna, id vestibulum sapien
+        ligula at lorem. Duis sagittis quis nunc imperdiet vulputate. Sed nunc
+        nisl, imperdiet congue tristique egestas, mollis et metus. Donec aliquam
+        molestie enim at viverra. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Fusce vel nunc id
+        augue dictum interdum.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
